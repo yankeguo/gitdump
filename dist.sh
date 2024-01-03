7eaa518b4cd3e85f1a462bca886bd1af79cdb778
GUO YANKE <hi@guoyk.xyz>
2024-01-03 11:27:17 +0800

    ci: add dist.sh

--- dist.sh
+++ dist.sh
@@ -6,24 +6,26 @@ cd "$(dirname "${0}")"
 
 rm -rf dist && mkdir dist
 
+EXECUTABLE_NAME="gitdump"
+
 build() {
   rm -rf build && mkdir build
-  GOOS=${1} GOARCH=${2} go build -o build/gitdump ./cmd/gitdump
-  tar -czvf "dist/gitdump-${1}-${2}.tar.gz" --exclude ".*" -C build gitdump
+  GOOS=${1} GOARCH=${2} go build -o "build/${EXECUTABLE_NAME}${3}"
+  tar -czvf "dist/${EXECUTABLE_NAME}-${1}-${2}.tar.gz" --exclude ".*" -C build "${EXECUTABLE_NAME}${3}"
   rm -rf build
 }
 
-build linux amd64
-build windows amd64
-build darwin amd64
-build darwin arm64
+build linux amd64 ""
+build windows amd64 ".exe"
+build darwin amd64 ""
+build darwin arm64 ""
 
-build linux arm64
-build linux loong64
-build windows 386
-build freebsd amd64
+build linux arm64 ""
+build linux loong64 ""
+build windows 386 ".exe"
+build freebsd amd64 ""
 
 cd dist
 
-shasum -a 256 *.tar.gz > SHASUM256.txt
+shasum -a 256 *.tar.gz >SHASUM256.txt
 gpg-trezor-hi@guoyk.xyz -ab SHASUM256.txt
